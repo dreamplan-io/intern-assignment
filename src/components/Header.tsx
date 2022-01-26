2ff941ec13ea52fa2a7518698c6dd9ebc9fb1620
Seb-pro <seba1199@edu.easj.dk>
2022-01-26 12:00:19 +0100

    mit forsøg

---
+++ src/components/Header.tsx
@@ -0,0 +1,9 @@
+function Header() {
+  return (
+    <header className="header">
+      <p className="text-xl font-medium text-white mx-10 my-10">Movielist</p>
+    </header>
+  );
+}
+
+export default Header;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,16 +1,13 @@
-import React from 'react';
-import Head from 'next/head';
+import React, { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 
 export default function Home() {
-  const API_KEY = 'paste your movie apikey here so we can easily test your solution with our own apikey'
-  
-  return (
-    <div>
-      <Head>
-        <title>Student assignment</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <p className="text-xl font-medium text-gray-800 mx-10 my-10">Movielist</p>
-    </div>
-  );
+  const router = useRouter();
+
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/movies');
+    });
+  });
+  return <div>Redirecting...</div>;
 }
